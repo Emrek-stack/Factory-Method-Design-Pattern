b14216ddbca98195febbb80f94d3a1b919c16126
Emre Karahan <emre.karahan@dogangazetecilik.com.tr>
2017-07-31 11:26:22 +0300

    commit

---
+++ FactoryDesingPatternRealWorl/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FactoryDesingPatternRealWorl
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Note: constructors call Factory Method
+            Document[] documents = new Document[2];
+
+            documents[0] = new Resume();
+            documents[1] = new Report();
+
+            foreach (Document document in documents)
+            {
+                Console.WriteLine("\n" + document.GetType().Name + "--");
+
+                foreach (Page page in document.Pages)
+                {
+                    Console.WriteLine(" " + page.GetType().Name);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Product' abstract class
+    /// </summary>
+    abstract class Page
+    {
+    }
+
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class SkillsPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class EducationPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class ExperiencePage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class IntroductionPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class ResultsPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class ConclusionPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class SummaryPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class BibliographyPage : Page
+    {
+    }
+
+    abstract class Document
+    {
+        private List<Page> _pages = new List<Page>();
+
+        //Constructor  calls abstract Factory Method
+        public Document()
+        {
+            this.CreatePages();
+        }
+
+        public List<Page> Pages
+        {
+            get { return _pages; }
+        }
+
+        // Factory Method
+        public abstract void CreatePages();
+    }
+
+
+    /// <summary>
+    /// A 'ConcreteCreator' class
+    /// </summary>
+    class Resume : Document
+    {
+        public override void CreatePages()
+        {
+            Pages.Add(new SkillsPage());
+            Pages.Add(new EducationPage());
+            Pages.Add(new ExperiencePage());
+        }
+    }
+
+    class Report : Document
+    {
+        public override void CreatePages()
+        {
+            Pages.Add(new IntroductionPage());
+            Pages.Add(new ResultsPage());
+            Pages.Add(new ConclusionPage());
+            Pages.Add(new SummaryPage());
+            Pages.Add(new BibliographyPage());
+        }
+    }
+}
